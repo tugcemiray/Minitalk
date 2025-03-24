630284f25b635c0842ea8292f4df7ec5e0dbfac4
tugcemiray <168101793+tugcemiray@users.noreply.github.com>
2025-03-24 19:52:32 +0300

    .
    .

---
+++ minitalk_bonus.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tukaraca <tukaraca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 03:30:41 by tukaraca          #+#    #+#             */
+/*   Updated: 2025/03/24 03:53:05 by tukaraca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
